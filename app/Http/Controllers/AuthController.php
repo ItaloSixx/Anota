7b474a35444f0edbe27898110bcf583d963d5943
ItaloSixx <anonimosbang@gmail.com>
2024-10-04 22:42:22 -0300

    show notes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,84 +5,77 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use PDOException;
 
 class AuthController extends Controller
 {
-    public function login(){
+    public function login()
+    {
         return view('login');
     }
 
-    //processes the login form submission
-    public function loginSubmit(Request $request){
-        //validation form
+    public function loginSubmit(Request $request)
+    {
+        // form validation
         $request->validate(
-            //rules
+            // rules
             [
                 'text_username' => 'required|email',
-                'text_password' => 'required|min:8|max:22'
+                'text_password' => 'required|min:6|max:16'
             ],
-            //errors
+            // error messages
             [
-                'text_username.required' => 'Necessário preencher o email',
-                'text_username.email' => 'Precisa ser um email válido',
-                'text_password.required' => 'Necessário preencher a senha',
-                'text_password.min' => 'Minimo de :min caracteres',
-                'text_password.max' => 'Maximo de :max caracteres'
-
+                'text_username.required' => 'O username é obrigatório',
+                'text_username.email' => 'Username deve ser um email válido',
+                'text_password.required' => 'A password é obrigatória',
+                'text_password.min' => 'A password deve ter pelo menos :min caracteres',
+                'text_password.max' => 'A password deve ter no máximo :max caracteres'
             ]
         );
+
+        // get user input
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        //queries the database for a user with the given username
+        // check if user exists
         $user = User::where('username', $username)
-                        ->where('deleted_at', NULL)
-                        ->first();
+                      ->where('deleted_at', NULL)
+                      ->first();
 
-        //if user is not found, redirects back with an error message
         if(!$user){
             return redirect()
-                    ->back()
-                    ->withInput()
-                    ->with('loginError', 'Usuário não encontrado');
+                   ->back()
+                   ->withInput()
+                   ->with('loginError', 'Username ou password incorretos.');
         }
 
-        //passoword check
+        // check if password is correct
         if(!password_verify($password, $user->password)){
             return redirect()
-                    ->back()
-                    ->withInput()
-                    ->with('loginError', 'Email ou senha incorretos');
-        }else{
-            //updates the last login timestamp
-            $user->last_login = date('Y-m-d H:i:s');
-            $user->save;
+                   ->back()
+                   ->withInput()
+                   ->with('loginError', 'Username ou password incorretos.');
         }
 
-        //login user
+        // update last login
+        $user->last_login = date('Y-m-d H:i:s');
+        $user->save();
+
+        // login user
         session([
-            'user' =>[
-                'id' =>$user->id,
+            'user' => [
+                'id' => $user->id,
                 'username' => $user->username
             ]
         ]);
 
+        // redirect to home
         return redirect()->to('/');
-
-
-
-        //get users database
-        //$userModel = new User();
-        //$users = $userModel->all()->toArray();
-        //echo '<pre>';
-        //print_r($users);
-
     }
 
-    public function logout(){
-    //logout
-    session()->forget('user');
-    return redirect()->to('/login');
+    public function logout()
+    {
+        // logout from the application
+        session()->forget('user');
+        return redirect()->to('/login');
     }
 }
--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -1,16 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index(){
         //load notes
+        $id = session('user.id');
+        $user = User::find($id)->toArray();
+        $notes = User::find($id)
+                       ->notes()
+                       ->get()
+                       ->toArray();
 
+        return view('home', ['notes' => $notes]);
 
-        return view('home');
     }
 
     public function newNote(){
